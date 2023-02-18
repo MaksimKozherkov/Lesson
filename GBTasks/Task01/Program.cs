1c628096730ce858565a41eebb081e46792a8182
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-18 19:31:03 +0300

    Решили начальную задачу

---
+++ GBTasks/Task01/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("Введите число: ");
+
+// int a=int.TryParse(ReadLine()!, out a);
+int a = Convert.ToInt32(ReadLine());
+WriteLine($"Квадрат числа {a} равен {a*a}");
