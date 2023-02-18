3eca518ed2b3d5dfdc4c85c7db0d7b2a14e24a4c
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-18 20:52:14 +0300

    Решил задачу

---
+++ GBTasks/Tasks/Task03/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+
+Clear();
+
+// будет выдавать название дня недели по номеру
+
+WriteLine("Введите номер дня недели: ");
+int a = Convert.ToInt32(ReadLine());
+
+if (a < 1 || a > 7)
+{
+    WriteLine("В неделе 7 дней!");
+}
+
+if (a == 1)
+{
+    WriteLine("Понедельник.");
+}
+
+if (a == 2)
+{
+    WriteLine("Вторник");
+}
+
+if (a == 3)
+{
+    WriteLine("Среда.");
+}
+
+if (a == 4)
+{
+    WriteLine("Четверг.");
+}
+
+if (a == 5)
+{
+    WriteLine("Пятница.");
+}
+
+if (a == 6)
+{
+    WriteLine("Суббота.");
+}
+
+if (a == 7)
+{
+    WriteLine("Воскресенье.");
+}
