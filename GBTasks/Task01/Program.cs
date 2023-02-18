f154a7d21d404603f45df4db4237ffa31ca169e6
MaksimKozherkov <121256228+MaksimKozherkov@users.noreply.github.com>
2023-02-18 20:49:32 +0300

    Delete GBTasks/Task01 directory

--- GBTasks/Task01/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using static System.Console;
-
-Clear();
-
-WriteLine("Введите число: ");
-
-// int a=int.TryParse(ReadLine()!, out a);
-int a = Convert.ToInt32(ReadLine());
-WriteLine($"Квадрат числа {a} равен {a*a}");
