5ccd479cb083b094c21962ca6bdef20e610271ad
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-18 20:52:35 +0300

    Решил задачу

---
+++ GBTasks/Tasks/Task04/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Clear();
+
+
+// напишите программу которая принимает на вход число n,и выводит все числа от 1 до n
+
+WriteLine("Введите число n: ");
+int n=Convert.ToInt32(ReadLine());
+int x=1;
+Write($"Числа от 1 до {n}: ");
+while(x<=n)
+{
+    Write($"{x} ");
+    x++;
+}
