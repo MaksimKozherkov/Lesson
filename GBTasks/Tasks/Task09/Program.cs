830d1e19f3ad7fcdbe38fff0a5f4b08bf1bc615e
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-19 14:44:48 +0300

    Решил задачу

---
+++ GBTasks/Tasks/Task09/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("Введите число a: ");
+int a=Convert.ToInt32(ReadLine()!);
+WriteLine("Введите число b: ");
+int b =Convert.ToInt32(ReadLine()!);
+
+
+WriteLine(a>b?$"{a}>{b}":$"{a}<{b}");
