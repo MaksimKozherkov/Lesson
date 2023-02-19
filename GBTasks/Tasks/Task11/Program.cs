1590851689670f517a4c767e53bed5f262926461
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-19 15:40:34 +0300

    Решил задачу

---
+++ GBTasks/Tasks/Task11/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+Clear();
+
+//принимает на вход 2 числа и проверяет является ли 1 крадратом второго
+
+WriteLine("Введите число a: ");
+int a=Convert.ToInt32(ReadLine()!);
+WriteLine("Введите число b: ");
+int b=Convert.ToInt32(ReadLine()!);
+
+WriteLine(a*a==b?$"Число {b} является квадратом числа {a}.":b*b==a?$"Число {a} является квадратом числа {b}.":$"Числа {a} и {b} не являются квадратами друг друга.");
