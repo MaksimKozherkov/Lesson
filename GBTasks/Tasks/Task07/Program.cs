8443e77098daa1101f98b86b12c23158dcda161d
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-19 13:35:29 +0300

    Решил задачу

---
+++ GBTasks/Tasks/Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+Clear();
+
+// напишите программу которая выводить случаайное 3х значное число и удаляет 2 цифру этого числа
+
+int n=new Random().Next(99,1000);
+WriteLine(n);
+Write(n/100);
+Write(n%10);
