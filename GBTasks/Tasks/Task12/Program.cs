307c084c25b78916d960f51edf6584ce885afe4a
Maksim Kozherkov <Mirex5915@gmail.com>
2023-02-19 16:34:20 +0300

    Решил задачу

---
+++ GBTasks/Tasks/Task12/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("Введите сколько банок сбил 1 стрелок: ");
+int a=Convert.ToInt32(ReadLine()!);
+WriteLine("Введите сколько банок сбыл 2 стрелок: ");
+int b=Convert.ToInt32(ReadLine()!);
+
+WriteLine($"Всего было {a+b-1} банок");
